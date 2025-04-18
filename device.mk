7d936b23bac1bd96acebf00d4d322adbb4067a3c
wulan17 <wulan17@komodos.id>
2025-04-18 15:30:20 +0700

    pond: Build libkeymaster_messages.vendor
    Signed-off-by: wulan17 <wulan17@komodos.id>

--- device.mk
+++ device.mk
@@ -94,9 +94,11 @@ PRODUCT_PACKAGES += \
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster4 \
     libkeymaster41 \
-    libpuresoftkeymasterdevice
+    libpuresoftkeymasterdevice \
+    libkeymaster_messages.vendor
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster_messages.so
